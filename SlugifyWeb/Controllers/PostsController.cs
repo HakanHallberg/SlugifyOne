b140414d22a09899ab3d5d798d7057de9320d7eb
Håkan Hallberg <hakan.hallberg.spo16@ecusolna.onmicrosoft.com>
2018-01-05 09:25:01 +0100

    Added logger

--- SlugifyWeb/Controllers/PostsController.cs
+++ SlugifyWeb/Controllers/PostsController.cs
@@ -28,6 +28,11 @@ namespace SlugifyWeb.Controllers
             return View(await _context.Posts.ToListAsync());
         }
 
+        public async Task<IActionResult> IndexByBlogg(int id)
+        {
+            return View("Index", await _context.Posts.Where(post => post.BloggID == id).ToListAsync());
+        }
+
         // GET: Posts/Details/5
         public async Task<IActionResult> Details(int? id)
         {
@@ -40,7 +45,7 @@ namespace SlugifyWeb.Controllers
                 .SingleOrDefaultAsync(m => m.PostID == id);
             if (post == null)
             {
-                _logger.LogWarning("Could not find with id =" + id.ToString());
+                _logger.LogWarning("Details: Could not find with id =" + id.ToString());
                 return NotFound();
             }
 
@@ -53,18 +58,24 @@ namespace SlugifyWeb.Controllers
             return View();
         }
 
+        public IActionResult CreateByBlogg(int id)
+        {
+            Post newPost = new Post { BloggID = id };
+            return View("Create", newPost);
+        }
+
         // POST: Posts/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("PostID,Title,ExplicitLanguage")] Post post)
+        public async Task<IActionResult> Create([Bind("PostID,Title,ExplicitLanguage,BloggID")] Post post)
         {
             if (ModelState.IsValid)
             {
                 _context.Add(post);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(IndexByBlogg), new { id = post.BloggID});
             }
             return View(post);
         }
--- SlugifyWeb/Data/DbSeeder.cs
+++ SlugifyWeb/Data/DbSeeder.cs
@@ -13,14 +13,21 @@ namespace SlugifyWeb.Data
             var bloggOne = new Blogg { Title = "A blogg about programming" };
             context.Bloggs.Add(bloggOne);
 
+            var coolPost = new Post { Blogg = bloggOne, Title = "Small CAPS@blah!" };
+            context.Posts.Add(coolPost);
+
             var bloggTwo = new Blogg { Title = "C#" };
             context.Bloggs.Add(bloggTwo);
+
+            var CSharp = new Post { Blogg = bloggTwo, Title = "Codes and stuff" };
+            context.Posts.Add(CSharp);
 
             var bloggThree = new Blogg { Title = "Java Script" };
             context.Bloggs.Add(bloggThree);
 
-            var coolPost = new Post { Blogg = bloggOne, Title = "Small CAPS@blah!" };
-            context.Posts.Add(coolPost);
+            var JavaScript = new Post { Blogg = bloggThree, Title = "Web stuff is cool!" };
+            context.Posts.Add(JavaScript);
+
 
             context.SaveChanges();
         }
